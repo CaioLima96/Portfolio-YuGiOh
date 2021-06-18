5f9b9c4094df3446b08c215dacf305a28f434349
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-06-18 17:08:33 -0300

    adicionado o owl carousel na home

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items: 5,
+      loop: true,
+      margin: 5,
+      autoplay: true,
+      autoplayTimeout: 1000,
+      dots: false,
+      autoplayHoverPause: true,
+      slideTransitions: 'ease',
+      responsive: {
+        0: {
+          items: 3
+        },
+        360: {
+          items: 4
+        },
+        450:{
+          items: 5
+        },
+        768:{
+          items: 5
+        },
+        1024: {
+          items: 6
+        }
+        
+    }
+
+    });
+  })
